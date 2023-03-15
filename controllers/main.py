9d905eb6d24e3a1973abc1fdd1f293b8299c3cc2
Başak kılıç <basak.9kilic@gmail.com>
2023-03-15 15:51:50 +0300

    Merge branch 'main' of https://github.com/MuhammedAba/contacts_stk

--- controllers/main.py
+++ controllers/main.py
@@ -8,3 +8,8 @@ class StkPage(http.Controller):
     def volunteer_webform(self, **kw):
         return http.request.render('contacts_stk.create_volunteer', {})
 
+    @http.route('/create/volunteer', type="http", auth="public", website=True)
+    def create_volunteer(self, **kw):
+        print("Data received...", kw)
+        request.env['res.partner'].sudo().create(kw)
+        return request.render("contacts_stk.volunteer_thanks", {})
--- models/res_partner.py
+++ models/res_partner.py
@@ -22,7 +22,7 @@ class Partner(models.Model):#orjinalini yaz
     partner_profession_workplace = fields.Char(string="Profession/Workplace")
     partner_sector = fields.Char(string="Sector")
     partner_languages = fields.Many2many("partner.languages", string="Languages")
-    partner_passport = fields.Selection([('yes', 'Yes'), ('no', 'No')], default= "Yes", string='Do you have a passport?')
+    partner_passport = fields.Selection([('yes', 'Yes'), ('no', 'No')], string='Do you have a passport?')
     partner_before_ngo = fields.Selection([('yes', 'Yes'), ('no', 'No')], string='Do you have an NGO that you are a member of?')
     partner_family_ngo = fields.Selection([('yes', 'Yes'), ('no', 'No')], string='Is there anyone in the family with an NGO?')
     partner_extra_info = fields.Char(string="Additional Notes")
