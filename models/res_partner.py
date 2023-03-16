5d08d65905e28a874b662e198313c0c38f206855
Başak kılıç <basak.9kilic@gmail.com>
2023-03-17 01:28:01 +0300

    languages -profession added,partner_identity removed

--- models/res_partner.py
+++ models/res_partner.py
@@ -7,7 +7,7 @@ class Partner(models.Model):#orjinalini yaz
     _description = 'Contact'
     _inherit = "res.partner"
 
-    partner_identity = fields.Char(String="Identification Number")
+    partner_job = fields.Char(String="Job")
     partner_member_types = fields.Many2one("partner.member.types", string="Member types")
     partner_blood_groups = fields.Many2one("partner.blood.groups", string="Blood Groups")
     partner_gender = fields.Selection([("male", "Male"), ("female", "Female")], string="Gender")
@@ -20,12 +20,12 @@ class Partner(models.Model):#orjinalini yaz
     # partner_birth_certificate = fields.Char(string="Birth Certificate")
     partner_driving_license_control = fields.Selection([('yes', 'Yes'), ('no', 'No')], string="Do you have driving license?")
     partner_driving_license = fields.Many2many("partner.driving.license", string="Driving License")
-    partner_education_status = fields.Many2one("partner.education.status", string="Education Status")
-    partner_profession_workplace = fields.Char(string="Profession/Workplace")
+    # partner_education_status = fields.Many2one("partner.education.status", string="Education Status")
+    # partner_profession = fields.Many2one("partner.profession", string="Profession")
     partner_sector = fields.Char(string="Sector")
     partner_languages = fields.Many2many("partner.languages", string="Languages")
     partner_passport = fields.Selection([('yes', 'Yes'), ('no', 'No')], string='Do you have a passport?')
     partner_before_ngo_control = fields.Selection([('yes', 'Yes'), ('no', 'No')], string="Do you have an NGO that you are a member of?")
-    partner_before_ngo = fields.Selection([('yes', 'Yes'), ('no', 'No')], string='Please Ngo enter.')
+    partner_before_ngo = fields.Selection([('yes', 'Yes'), ('no', 'No')], string='Please enter Ngo name.')
     partner_family_ngo = fields.Selection([('yes', 'Yes'), ('no', 'No')], string='Is there anyone in the family with an NGO?')
     partner_extra_info = fields.Char(string="Additional Notes")
