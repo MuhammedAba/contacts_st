4a7a108c1b7139e5db8022501ea10e4455ccf967
Muhammed <talhaaba456@gmail.com>
2023-03-21 13:20:30 +0300

    codes edited

--- models/res_partner.py
+++ models/res_partner.py
@@ -15,16 +15,14 @@ class Partner(models.Model):#orjinalini yaz
     partner_city = fields.Char(string="City")
     partner_contact_choice_control = fields.Selection([("email", "E-mail"), ("phone", "Phone")], string="How can we contact?")
 
-    #TAB1 Extra İnformation
-    # partner_birth_place = fields.Char( string="Place Of Birth")
-    # partner_birth_certificate = fields.Char(string="Birth Certificate")
-    partner_driving_license_control = fields.Selection([('yes', 'Yes'), ('no', 'No')], string="Do you have driving license?")
+    #TAB1 Extra Information
+    partner_languages = fields.Many2many("partner.languages", string="Languages")
     partner_driving_license = fields.Many2many("partner.driving.license", string="Driving License")
     partner_education_status = fields.Many2one("partner.education.status", string="Education Status")
-    partner_profession_workplace = fields.Char(string="Profession/Workplace")
-    partner_sector = fields.Char(string="Sector")
-    partner_languages = fields.Many2many("partner.languages", string="Languages")
+    partner_driving_license_control = fields.Selection([('yes', 'Yes'), ('no', 'No')], string="Do you have driving license?")
     partner_passport = fields.Selection([('yes', 'Yes'), ('no', 'No')], string='Do you have a passport?')
     partner_before_ngo = fields.Selection([('yes', 'Yes'), ('no', 'No')], string='Do you have an NGO that you are a member of?')
     partner_family_ngo = fields.Selection([('yes', 'Yes'), ('no', 'No')], string='Is there anyone in the family with an NGO?')
+    partner_profession_workplace = fields.Char(string="Profession/Workplace")
+    partner_sector = fields.Char(string="Sector")
     partner_extra_info = fields.Char(string="Additional Notes")
