a8c21ecbdfb6a689e5f59d205a7284112b131a20
Muhammed Aba <talhaaba456@gmail.com>
2023-03-23 17:11:10 +0300

    added education status to website

--- controllers/main.py
+++ controllers/main.py
@@ -7,6 +7,7 @@ class StkPage(http.Controller):
     @http.route('/volunteer_webform', type="http", auth='public', website=True)
     def volunteer_webform(self, **kw):
         member_rec = request.env["partner.member.types"].sudo().search([])  # Name of model
+        member_rec = request.env["partner.member.types"].sudo().search([])  # Name of model
         education_rec = request.env["partner.education.status"].sudo().search([])  # Name of model
         return http.request.render('contacts_stk.create_volunteer', {'education_rec': education_rec, 'member_rec': member_rec})  # ModuleName.TemplateId
 
