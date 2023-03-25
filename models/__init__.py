035ad045827244693a9809e3f726e130e7e31eae
Muhammed Aba <talhaaba456@gmail.com>
2023-03-25 16:49:27 +0300

    fixed problem of education status

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 #
-from . import partner_blood_groups , partner_driving_license , partner_languages ,partner_member_types ,partner_sector, res_partner
+from . import partner_blood_groups, partner_driving_license, \
+    partner_languages, partner_member_types, partner_sector, partner_education_status, res_partner
 
