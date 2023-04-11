862a6d870f22073473f0bb718e0585ce948d8073
Başak kılıç <125957934+basakiliic@users.noreply.github.com>
2023-04-11 12:02:27 +0300

    Update res_partner.py

--- models/res_partner.py
+++ models/res_partner.py
@@ -7,7 +7,7 @@ class Partner(models.Model):  # orjinalini yaz
     _description = 'Contact'
     _inherit = "res.partner"
 
-    partner_member_types = fields.Many2one("partner.member.types", string="Member types")
+    partner_member_types = fields.Many2one("partner.member.types", string="Member Types")
     partner_blood_groups = fields.Many2one("partner.blood.groups", string="Blood Groups")
     partner_gender = fields.Selection([("male", "Male"), ("female", "Female")], string="Gender")
     partner_birth_date = fields.Date(string="Birth Date")
